1b8daea9ab2005245499b42fde4b9ba81017638e
Andre Fernandes <andre.fernandes86@gmail.com>
2022-07-27 18:28:12 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -26,7 +26,7 @@ case $choice in
   1)  echo "You have selected the option 1"
     clear
     echo '--------------------------'
-    cd scripts
+    #cd scripts
     python3 v1_isolate-endpoint.py
     read -p "Press any key to continue.."
     sleep 5
@@ -45,7 +45,7 @@ case $choice in
   2)  echo "You have selected the option 2"
     clear
     echo '---------------------------'
-    cd scripts
+    #cd scripts
     python3 v1_isolate-endpoint-name.py
     read -p "Press any key to continue.."
     sleep 5
@@ -64,7 +64,7 @@ case $choice in
   3)  echo "You have selected the option 3"
     clear
     echo '-----------------------------------'
-    cd scripts
+    #cd scripts
     python3 v1-restore-endpoint-connection.py
     read -p "Press any key to continue.."
     sleep 5
@@ -83,7 +83,7 @@ case $choice in
   4)  echo "You have selected the option 4"
     clear
     echo '-----------------------------------'
-    cd scripts
+    #cd scripts
     python3 v1-getResponseStatus.py
     read -p "Press any key to continue.."
     sleep 5
