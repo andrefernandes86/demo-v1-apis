dabd7b52c9d60f306e36932f7480ff012aaba645
Andre Fernandes <andre.fernandes86@gmail.com>
2022-07-27 17:49:01 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -21,7 +21,7 @@ echo "3. Restore the connection of an Isolated Endpoint"
 echo "4. Check the Response Status"
 echo "5. Exit from menu "
 echo "--------------------------------------------------------------------------------------------------------------------";
-echo -n "Enter your menu choice [1-4]: "
+echo -n "Enter your menu choice [1-5]: "
 
 # Preparing the Application 
 while :
@@ -36,182 +36,37 @@ case $choice in
   1)  echo "You have selected the option 1"
     clear
     echo '--------------------------'  
-    echo '---------- NMAP ----------'
-    echo 'Target IP Address: '
-    read NMAPIP 
-    echo ' ' 
-    echo 'Running the NMAP Scan..'  
-    docker run --rm -it andrefernandes86/tools-nmap -A -T4 $NMAPIP
-    echo ' ' 
+    cd scripts/
+    python3 v1_isolate-endpoint.py
     read -p "Press any key to continue.."
     sleep 3
-    clear
-    echo "1. Port Scan"
-    echo "2. Brute Force Tool"
-    echo "3. Web Application Tools"
-    echo "4. Domain Scan"
-    echo "5. Generating Malware Samples (Real Samples)"
-    echo "6. Generating a Vulnerable Web Application"
-    echo "7. Install Docker "
-    echo "8. Exit from menu "
-    echo -n "Enter your menu choice [1-8]: "
     echo "";;
   # Option 2
   2)  echo "You have selected the option 2"
     clear
     echo '---------------------------'  
-    echo '---------- HYDRA ----------'
-    echo 'Target IP Address: '
-    read HYDRAIP 
-    echo ' ' 
-    echo 'Target Port: '
-    read HYDRAPORT 
-    echo ' ' 
-    echo 'Running the HYDRA Brute Force..'  
-    docker run --rm andrefernandes86/tools-hydra -L /opt/usernames/top-usernames-shortlist.txt -P /opt/passwords/darkweb2017-top100.txt -f -V -s $HYDRAPORT $HYDRAIP http-get
-    echo ' ' 
+    python3 v1_isolate-endpoint-name.py
     read -p "Press any key to continue.."
     sleep 3
     clear
-    echo "1. Port Scan"
-    echo "2. Brute Force Tool"
-    echo "3. Web Application Tools"
-    echo "4. Domain Scan"
-    echo "5. Generating Malware Samples (Real Samples)"
-    echo "6. Generating a Vulnerable Web Application"
-    echo "7. Install Docker "
-    echo "8. Exit from menu "
-    echo -n "Enter your menu choice [1-8]: "
     echo "";;
     # Option 3
   3)  echo "You have selected the option 3"
     clear
     echo '-----------------------------------'  
-    echo '---------- Web App Tools ----------'
-    echo ' ' 
-    echo 'Running the Web App Tools..'  
-    docker run --rm -d --name webtools -p 222:22 -p 7331:7331 -p 9292:9292 arachni/arachni:latest
-    echo ' ' 
-    echo ' URL: http://$ip-address:9292 '
-    echo ' Username: admin@admin.admin '
-    echo ' Password: administrator '
-    echo ' ' 
+    python3 v1-getResponseStatus.py
     read -p "Press any key to continue.."
-    echo ' ' 
     sleep 3
-    docker kill webtools
-    clear
-    echo "1. Port Scan"
-    echo "2. Brute Force Tool"
-    echo "3. Web Application Tools"
-    echo "4. Domain Scan"
-    echo "5. Generating Malware Samples (Real Samples)"
-    echo "6. Generating a Vulnerable Web Application"
-    echo "7. Install Docker "
-    echo "8. Exit from menu "
-    echo -n "Enter your menu choice [1-8]: "
     echo "";;
   # Option 4
   4)  echo "You have selected the option 4"
     clear
     echo '-----------------------------------'  
-    echo '---------- Domain Scan ----------'
-    echo ' ' 
-    echo 'Running the Domain Scan #1'  
-    echo 'Target Domain: '
-    read NETIP 
-    echo ' ' 
-    echo 'Target Port: '
-    read NETPORT 
-    echo ' ' 
-    docker run --rm -it --privileged --net=host -e TARGET_DOMAIN=$NETIP:$NETPORT andrefernandes86/tools-pentest
-    echo ' ' 
-    clear
-    echo 'Running the Domain Scan #2'  
-    docker run --rm -it andrefernandes86/tools-domain-scan bash
-    read -p "Press any key to continue.."
-    echo ' ' 
-    sleep 3
-    clear
-    echo "1. Port Scan"
-    echo "2. Brute Force Tool"
-    echo "3. Web Application Tools"
-    echo "4. Domain Scan"
-    echo "5. Generating Malware Samples (Real Samples)"
-    echo "6. Generating a Vulnerable Web Application"
-    echo "7. Install Docker "
-    echo "8. Exit from menu "
-    echo -n "Enter your menu choice [1-8]: "
-    echo "";;
-  # Option 5
-  5)  echo "You have selected the option 5"
-    clear
-    echo '-------------------------------------'  
-    echo '---------- Malware Samples ----------'
-    echo ' ' 
-    echo 'Creating the webserver and adding the malware samples..'  
-    docker run --rm -d --name malware -p 80:80 andrefernandes86/tools-malware-samples
-    echo ' ' 
-    echo ' URL: http://$ip-address '
-    echo ' ' 
+    python3 v1-restore-endpoint-connection.py
     read -p "Press any key to continue.."
     sleep 3
-    docker kill malware
-    clear
-    echo "1. Port Scan"
-    echo "2. Brute Force Tool"
-    echo "3. Web Application Tools"
-    echo "4. Domain Scan"
-    echo "5. Generating Malware Samples (Real Samples)"
-    echo "6. Generating a Vulnerable Web Application"
-    echo "7. Install Docker "
-    echo "8. Exit from menu "
-    echo -n "Enter your menu choice [1-8]: "
     echo "";;
-    # Option 6
-  6)  echo "You have selected the option 6"
-    clear
-    echo '------------------------------------------------'  
-    echo '---------- Vulnerable Web Application ----------'
-    echo ' ' 
-    echo 'Creating the vulnerable webserver..'  
-    docker run --rm -d --name wsrv -p 81:80 andrefernandes86/demo-aws-c1ns-vulnerable-webserver
-    echo ' ' 
-    echo ' URL: http://$ip-address:81 '
-    echo ' Username: admin '
-    echo ' Password: password '
-    echo ' ' 
-    read -p "Press any key to continue.."
-    sleep 3
-    docker kill wsrv
-    clear
-    echo "1. Port Scan"
-    echo "2. Brute Force Tool"
-    echo "3. Web Application Tools"
-    echo "4. Domain Scan"
-    echo "5. Generating Malware Samples (Real Samples)"
-    echo "6. Generating a Vulnerable Web Application"
-    echo "7. Install Docker "
-    echo "8. Exit from menu "
-    echo -n "Enter your menu choice [1-8]: "
-    echo "";;
-    7)  echo "You have selected the option 7"
-    clear
-    apt-get update -y
-    curl -fsSL https://get.docker.com | sh
-    clear
-    echo "1. Port Scan"
-    echo "2. Brute Force Tool"
-    echo "3. Web Application Tools"
-    echo "4. Domain Scan"
-    echo "5. Generating Malware Samples (Real Samples)"
-    echo "6. Generating a Vulnerable Web Application"
-    echo "7. Install Docker "
-    echo "8. Exit from menu "
-    echo -n "Enter your menu choice [1-8]: "
-    echo "";;
-  # Option 7
-  8)  echo "Quitting ..."
+  5)  echo "Quitting ..."
     echo ' '
       exit;;
   
@@ -219,5 +74,5 @@ case $choice in
   *) echo "invalid option";;
   
 esac
-  echo -n "Enter your menu choice [1-8]: "
+  echo -n "Enter your menu choice [1-5]: "
 done
