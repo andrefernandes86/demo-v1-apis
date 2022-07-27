be71fea768025118270b2e2e1f77cafa7dcafe45
Andre Fernandes <andre.fernandes86@gmail.com>
2022-07-27 18:04:20 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 clear
-echo "---------------------------------------- Trend Micro Vision One - API Tools ----------------------------------------";
-echo ' ';
-echo ' Please, provide the Vision One API Key: ';
-read tokenid
-echo ' ';
-sleep 5
-clear
 
 # Menu Options
 echo "---------------------------------------- Trend Micro Vision One - API Tools ----------------------------------------";
 echo ' ';
-echo ' Your API Key: $tokenid';
-echo ' ';
-echo "--------------------------------------------------------------------------------------------------------------------";
 echo "1. Isolate an Endpoint based on GUI"
 echo "2. Isolate an Endpoint based on Hostname"
 echo "3. Restore the connection of an Isolated Endpoint"
 echo "4. Check the Response Status"
 echo "5. Exit from menu "
+echo ' ';
 echo "--------------------------------------------------------------------------------------------------------------------";
 echo -n "Enter your menu choice [1-5]: "
 
@@ -39,6 +30,16 @@ case $choice in
     python3 v1_isolate-endpoint.py
     read -p "Press any key to continue.."
     sleep 3
+    clear
+    echo "---------------------------------------- Trend Micro Vision One - API Tools ----------------------------------------";
+    echo ' ';
+    echo "1. Isolate an Endpoint based on GUI"
+    echo "2. Isolate an Endpoint based on Hostname"
+    echo "3. Restore the connection of an Isolated Endpoint"
+    echo "4. Check the Response Status"
+    echo "5. Exit from menu "
+    echo ' ';
+    echo "--------------------------------------------------------------------------------------------------------------------";
     echo "";;
   # Option 2
   2)  echo "You have selected the option 2"
@@ -49,6 +50,15 @@ case $choice in
     read -p "Press any key to continue.."
     sleep 3
     clear
+    echo "---------------------------------------- Trend Micro Vision One - API Tools ----------------------------------------";
+    echo ' ';
+    echo "1. Isolate an Endpoint based on GUI"
+    echo "2. Isolate an Endpoint based on Hostname"
+    echo "3. Restore the connection of an Isolated Endpoint"
+    echo "4. Check the Response Status"
+    echo "5. Exit from menu "
+    echo ' ';
+    echo "--------------------------------------------------------------------------------------------------------------------";
     echo "";;
     # Option 3
   3)  echo "You have selected the option 3"
@@ -58,6 +68,15 @@ case $choice in
     python3 v1-getResponseStatus.py
     read -p "Press any key to continue.."
     sleep 3
+    echo "---------------------------------------- Trend Micro Vision One - API Tools ----------------------------------------";
+    echo ' ';
+    echo "1. Isolate an Endpoint based on GUI"
+    echo "2. Isolate an Endpoint based on Hostname"
+    echo "3. Restore the connection of an Isolated Endpoint"
+    echo "4. Check the Response Status"
+    echo "5. Exit from menu "
+    echo ' ';
+    echo "--------------------------------------------------------------------------------------------------------------------";    
     echo "";;
   # Option 4
   4)  echo "You have selected the option 4"
@@ -67,6 +86,15 @@ case $choice in
     python3 v1-restore-endpoint-connection.py
     read -p "Press any key to continue.."
     sleep 3
+    echo "---------------------------------------- Trend Micro Vision One - API Tools ----------------------------------------";
+    echo ' ';
+    echo "1. Isolate an Endpoint based on GUI"
+    echo "2. Isolate an Endpoint based on Hostname"
+    echo "3. Restore the connection of an Isolated Endpoint"
+    echo "4. Check the Response Status"
+    echo "5. Exit from menu "
+    echo ' ';
+    echo "--------------------------------------------------------------------------------------------------------------------";    
     echo "";;
   5)  echo "Quitting ..."
     echo ' '
