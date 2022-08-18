e4086c53bb15810100688e29e2b04bee9daf8f24
Andre Fernandes <andre.fernandes86@gmail.com>
2022-08-18 14:30:21 -0500

    Create v1-solist.py

---
+++ v1-solist.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+url_base = 'https://api.xdr.trendmicro.com'
+url_path = '/v2.0/xdr/threatintel/suspiciousObjects'
+
+
+tokenid = input('Please provide the VisionOne API Key?\n')
+typeid = input('What kind of IOC do you want to add to the SO list? (ip, url, sha1)\n\n')
+valueid = input('What is the IOC do you want to add to the SO list?\n\n')
+scanActionid = input('What action do you want to define as a reponse anytime this IOC is detected by the Trend Micro solutions?\n\n')
+riskLevelid = input('How do you classify this IOC? If High-Risk, plase use "high", if Medium-Risk, use "medium", and for Low-Risk, use "low".\n\n')
+
+query_params = {}
+headers = {'Authorization': 'Bearer ' + tokenid,
+    'Content-Type': 'application/json;charset=utf-8'}
+body = {'data': [{'riskLevel': riskLevelid,
+              'scanAction': scanActionid,
+              'type': typeid,
+              'value': valueid}]}
+
+r = requests.post(url_base + url_path, params=query_params, headers=headers, json=body)
+
+print(r.status_code)
+if 'application/json' in r.headers.get('Content-Type', '') and len(r.content):
+    print(json.dumps(r.json(), indent=4))
+else:
+    print(r.text)
